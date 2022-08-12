4107ba544e55d4731f45f01a020e69d58f9471af
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2022-08-12 08:28:15 +0300

    funny rat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+enum CellState {
+    Empty,
+    O,
+    X
+}
+
+type Field = {
+    cells: CellState[]
+}
+
+type CellProps = {
+    cellState: CellState
+}
+
+function Cell({cellState} : CellProps) {
+    const imagesToUse = {
+        [CellState.Empty]: <div className="cell empty-cell"/>,
+        [CellState.O]: <div className="cell o-cell"/>,
+        [CellState.X]: <div className="cell x-cell"/>
+    }
+
+    return imagesToUse[cellState];
+}
+
+function Field() {
+    const [cells, setCells] = useState(new Array(9).fill(CellState.X))
+
+    return (
+        <div className="field">
+            {
+                cells.map((cellState, i) => <Cell key={i} cellState={cellState}/>)
+            }
+        </div>
+    );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Field/>
+        </div>
+    )
 }
 
 export default App;

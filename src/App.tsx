2db3f351e2ca6a2613a1c1d41aa5a0a57d68c5d3
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2022-08-20 14:50:46 +0300

    Basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 
+const SIZE = 3;
+
 enum CellState {
     Empty,
     O,
@@ -12,34 +14,53 @@ type Field = {
 }
 
 type CellProps = {
-    cellState: CellState
+    cellState: CellState,
+    index: number,
+    cellClick: (index: number) => void
 }
 
-function Cell({cellState} : CellProps) {
+function Cell({cellState, index, cellClick} : CellProps) {
     const imagesToUse = {
-        [CellState.Empty]: <div className="cell empty-cell"/>,
-        [CellState.O]: <div className="cell o-cell"/>,
-        [CellState.X]: <div className="cell x-cell"/>
+        [CellState.Empty]: <td className="cell empty-cell" onClick={() => cellClick(index)}>Click to place</td>,
+        [CellState.O]: <td className="cell o-cell"/>,
+        [CellState.X]: <td className="cell x-cell"/>
     }
 
     return imagesToUse[cellState];
 }
 
 function Field() {
-    const [cells, setCells] = useState(new Array(9).fill(CellState.X))
+    const [cells, setCells] = useState(new Array(9).fill(CellState.Empty));
+
+    function cellClick(index: number) {
+        const unfilledCells = cells.filter(x => x == CellState.Empty).length;
+        
+        const changeTo = (unfilledCells % 2 ? CellState.O : CellState.X);
+
+        setCells(currentCells => {
+            return [...currentCells.slice(0, index), changeTo, ...currentCells.slice(index+1)];
+        });
+
+        // TODO:game win logic
+    }
 
     return (
-        <div className="field">
+        <table className="field">
             {
-                cells.map((cellState, i) => <Cell key={i} cellState={cellState}/>)
+                new Array(SIZE).fill(0).map((v, i) => (
+                    <tr key={i}>
+                        {cells.slice(i*SIZE, (i+1)*SIZE).map((v, j) => <Cell cellState={v} index={i*SIZE + j} cellClick={cellClick}/>)}
+                    </tr>
+                ))
             }
-        </div>
+        </table>
     );
 }
 
 function App() {
     return (
         <div className="app">
+            <h1>Mouse Game</h1>
             <Field/>
         </div>
     )
